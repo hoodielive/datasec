99f0bc2a1246751353db8fba7919369bad10c7fc
hoodielive <hoodielive@gmail.com>
2019-07-15 18:23:21 -0400

    add a better ui

--- python/mac-changer/macdaddy.py
+++ python/mac-changer/macdaddy.py
@@ -7,11 +7,11 @@ import optparse
 parser = optparse.OptionParser()
 parser.add_option('-i', '--interface', dest='interface', help='interface to change MAC address.')
 parser.add_option('-m', '--mac', dest='new_mac', help='new MAC address')
-parser.parse_args()
+(options, arguments) = parser.parse_args()
 
 # ui
-interface = input("interface > ")
-new_mac = input("new Mac > ")
+interface = options.interface
+new_mac = options.new_mac
 
 print(f'[+] Changing MAC address for {interface} to {new_mac}.')
 
