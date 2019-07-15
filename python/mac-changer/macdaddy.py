bbd4493510fd26f56c890fdac700beeb2d1ddde8
hoodielive <hoodielive@gmail.com>
2019-07-15 18:28:37 -0400

    refactor ui and put work in function

--- python/mac-changer/macdaddy.py
+++ python/mac-changer/macdaddy.py
@@ -3,6 +3,12 @@
 import subprocess
 import optparse
 
+def change_mac(interface, new_mac):
+    print(f'[+] Changing MAC address for {interface} to {new_mac}.')
+    subprocess.call(['ifconfig', interface, 'down'])
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+    subprocess.call(['ifconfig', interface, 'up'])
+        
 # parser object/entity 
 parser = optparse.OptionParser()
 parser.add_option('-i', '--interface', dest='interface', help='interface to change MAC address.')
@@ -10,11 +16,4 @@ parser.add_option('-m', '--mac', dest='new_mac', help='new MAC address')
 (options, arguments) = parser.parse_args()
 
 # ui
-interface = options.interface
-new_mac = options.new_mac
-
-print(f'[+] Changing MAC address for {interface} to {new_mac}.')
-
-subprocess.call(['ifconfig', interface, 'down'])
-subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
-subprocess.call(['ifconfig', interface, 'up'])
+change_mac(options.interface, options.new_mac)
