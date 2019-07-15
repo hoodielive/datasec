ce33c0989c9a5dd5abda5488431adc5f2bc423a4
hoodielive <hoodielive@gmail.com>
2019-07-15 11:03:17 -0400

    added sheilding to combat user dickery

--- python/mac-changer/macdaddy.py
+++ python/mac-changer/macdaddy.py
@@ -2,11 +2,10 @@
 
 import subprocess
 interface = input("interface > ")
-#interface = "wlan0"
 state =['down', 'up']
 new_mac = input("new Mac > ")
 
 print(f'[+] Changing MAC address for {interface} to {new_mac}.')
-subprocess.call(f'ifconfig {interface} {state[0]}', shell=True)
-subprocess.call(f'ifconfig {interface} hw ether {new_mac}', shell=True)
-subprocess.call(f'ifconfig {interface} {state[1]}', shell=True)
+subprocess.call(['ifconfig', interface, 'down'])
+subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+subprocess.call(['ifconfig', interface, 'up'])
