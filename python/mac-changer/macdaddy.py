872282d13786c549640f7c6963751ded76d5378a
hoodielive <hoodielive@gmail.com>
2019-07-14 21:53:10 -0400

    macdaddy - mac changer - executable

--- python/mac-changer/macdaddy.py
+++ python/mac-changer/macdaddy.py
@@ -6,7 +6,6 @@ state =['down', 'up']
 new_mac = "00:22:33:44:55:77"
 
 print(f'[+] Changing MAC address for {interface} to {new_mac}.')
-
 subprocess.call(f'ifconfig {interface} {state[0]}', shell=True)
 subprocess.call(f'ifconfig {interface} hw ether {new_mac}', shell=True)
 subprocess.call(f'ifconfig {interface} {state[1]}', shell=True)
