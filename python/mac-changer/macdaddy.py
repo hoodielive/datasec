8173f8ee23e229459671a681b2a5f0777d16180f
hoodielive <hoodielive@gmail.com>
2019-07-15 18:52:36 -0400

    bringing this to a close lol

--- python/mac-changer/macdaddy.py
+++ python/mac-changer/macdaddy.py
@@ -8,11 +8,11 @@ def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option('-i', '--interface', dest='interface', help='interface to change MAC address.')
     parser.add_option('-m', '--mac', dest='new_mac', help='new MAC address')
+    (options, arguments) = parser.parse_args()
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info.")
     elif not options.new_mac:
         parser.error("[-] Please specify an new_mac, use --help for more info.")
-    else: 
     return options
 
 
