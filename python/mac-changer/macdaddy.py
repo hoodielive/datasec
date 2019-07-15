2d94f147ee9297fe2656847c67688f2ac664a2de
hoodielive <hoodielive@gmail.com>
2019-07-14 20:54:48 -0400

    update macdaddy

--- python/mac-changer/macdaddy.py
+++ python/mac-changer/macdaddy.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 import subprocess
+interface = "wlan0"
+command = "ifconfig"
+state =['down', 'up']
+new_mac = "00:22:33:44:55:77"
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:66 ", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
-#subprocess.call("ifconfig", shell=True)
+print(f'[+] Changing MAC address for {interface} to {new_mac}.')
+
+subprocess.call(f'ifconfig {interface} {state[0]}', shell=True)
+subprocess.call(f'ifconfig {interface} hw ether {new_mac}', shell=True)
+subprocess.call(f'ifconfig {interface} {state[1]}', shell=True)
