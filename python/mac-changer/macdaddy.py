9ae07ed037725af6cf310945df982aa0d861325f
hoodielive <hoodielive@gmail.com>
2019-07-15 22:04:49 -0400

    work more on the checking

--- python/mac-changer/macdaddy.py
+++ python/mac-changer/macdaddy.py
@@ -9,6 +9,8 @@ def get_arguments():
     parser.add_option('-i', '--interface', dest='interface', help='interface to change MAC address.')
     parser.add_option('-m', '--mac', dest='new_mac', help='new MAC address')
     (options, arguments) = parser.parse_args()
+
+    # check to see if proper args are supplied
     if not options.interface:
         parser.error("[-] Please specify an interface, use --help for more info.")
     elif not options.new_mac:
@@ -22,6 +24,7 @@ def change_mac(interface, new_mac):
     subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
     subprocess.call(['ifconfig', interface, 'up'])
         
-#(options, arguments) = get_arguments()
 options = get_arguments()
-change_mac(options.interface, options.new_mac)
+
+ifconfig_result = subprocess.check_output(["ifconfig", options.interface]) 
+print(ifconfig_result)
