444574f703c6c466230f197a04aa75a3df242d1f
hoodielive <hoodielive@gmail.com>
2019-07-16 10:49:42 -0400

    dunno

--- python/network-scanner/network_scanner.py
+++ python/network-scanner/network_scanner.py
@@ -2,12 +2,12 @@
 
 import scapy.all as scapy
 
-def scan(ip): 
+def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     arp_request.show()
     broadcast = scapy.Ether("ff:ff:ff:ff:ff:ff")
     broadcast.show()
-    arp_request_broadcast = broadcast/arp_request 
+    arp_request_broadcast = broadcast/arp_request
     print(arp_request_broadcast.summary())
     arp_request_broadcast.show()
 
