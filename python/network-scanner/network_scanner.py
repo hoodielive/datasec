1d9c9f6986d95761ad0d6cb32b95f39170a3d3a8
hoodielive <hoodielive@gmail.com>
2019-07-16 07:49:30 -0400

    whois

--- python/network-scanner/network_scanner.py
+++ python/network-scanner/network_scanner.py
@@ -3,8 +3,7 @@
 import scapy.all as scapy
 
 def scan(ip): 
-    arp_request = scapy.ARP()
+    arp_request = scapy.ARP(pdst=ip)
     print(arp_request.summary())
-    scapy.ls(scapy.ARP()) 
 
 scan('10.0.1.1/24')
