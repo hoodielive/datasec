c9937dca66f9d265fe07cd27bc028087f485114b
hoodielive <hoodielive@gmail.com>
2019-07-15 22:47:58 -0400

    sweet

--- python/mac-changer/macdaddy.py
+++ python/mac-changer/macdaddy.py
@@ -37,4 +37,10 @@ def get_current_mac(interface):
 options = get_arguments()
 current_mac = get_current_mac(options.interface)
 print("Current mac is:", current_mac)
+change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
 
+if current_mac == options.new_mac:
+    print('[+] MAC address was successfully change to:', current_mac)
+else:
+    print('[-] MAC address did not get changed.')
