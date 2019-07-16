6bdb741fa9c9550ccf843b2d8ee969e561d410be
hoodielive <hoodielive@gmail.com>
2019-07-16 07:54:30 -0400

    figure out the host via broadcast

--- python/network-scanner/network_scanner.py
+++ python/network-scanner/network_scanner.py
@@ -4,6 +4,7 @@ import scapy.all as scapy
 
 def scan(ip): 
     arp_request = scapy.ARP(pdst=ip)
-    print(arp_request.summary())
+    broadcast = scapy.Ether()
+    scapy.ls(broadcast)
 
 scan('10.0.1.1/24')
